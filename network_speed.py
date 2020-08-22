500a5e79710b0b7fc74c80b25f548a32230b721d
Phil Roli <philipprolinek@gmail.com>
2020-08-22 22:00:48 +0200

    Network Speed

---
+++ network_speed.py
@@ -0,0 +1,22 @@
+import os
+import speedtest
+from tabulate import tabulate
+
+class Network(object):
+    def __init__(self):
+        self.parser = speedtest.Speedtest()
+
+    def data(self):
+        down = str(f"{round(self.parser.download() / 1_000_000, 2)} Mbps")
+        print("Dowload Done")
+        up = str(f"{round(self.parser.upload() / 1_000_000, 2)} Mbps")
+        return [["Interface", "Download", "Upload"], ["TP-LINK_1234", down, up]]
+    
+    def __repr__(self):
+        speed = self.data()
+        os.system('cls')
+        return tabulate(speed, headers="firstrow", tablefmt="fancy_grid")
+
+if __name__ == "__main__":
+    print(Network())
+    input()
