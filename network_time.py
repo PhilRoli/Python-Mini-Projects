14a920887bc1a8fb7031d1afe24a90ae71ccd398
Phil Roli <philipprolinek@gmail.com>
2020-08-23 21:59:58 +0200

    e

---
+++ network_time.py
@@ -0,0 +1,30 @@
+import speedtest
+from tabulate import tabulate
+import time
+
+class Network(object):
+    def __init__(self):
+        self.parser = speedtest.Speedtest()
+
+    def data(self):
+        Tstart = time.time()
+        down = str(f"{round(self.parser.download() / 1_000_000, 2)} Mbps")
+        up = str(f"{round(self.parser.upload() / 1_000_000, 2)} Mbps")
+        Tend = time.time()
+        Tfinal = Tend - Tstart
+        return [["Time", "Download", "Upload"], [Tfinal, down, up]]
+    
+    def __repr__(self):
+        speed = self.data()
+        return tabulate(speed, headers="firstrow", tablefmt="simple")
+
+def timetest():
+    anzahl = input("Anzahl an durchläufen: ")
+    i = int(anzahl)
+    print(i)
+    while i > 0:
+        print(Network())
+        i=-1
+
+if __name__ == "__main__":
+    timetest()
